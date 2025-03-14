7b7ea43540f1d9c06553ed68d2ded5c40923baef
Clément Ribé-Müller <clement@pixxid.fr>
2025-03-14 10:07:36 +0100

    fix(capacityPlan) Correction publication

--- lib/src/capacity_plan/capacity_plan.dart
+++ lib/src/capacity_plan/capacity_plan.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import 'dart:convert';
-
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // Widgets
